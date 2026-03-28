b658352d2b3a5ab52f31c6e715adc4e527c267ad
ashwiniajgekar <ashwiniajgekar@gmail.com>
2026-03-28 12:50:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,18 +5,7 @@ function App() {
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetch("http://localhost:3000/")
-      .then((res) => res.text())
-      .then((data) => {
-        setMessage(data);
-        setLoading(false);
-      })
-      .catch(() => {
-        setMessage("Error connecting to server");
-        setLoading(false);
-      });
-  }, []);
+
 
   return (
     <div className="container">
